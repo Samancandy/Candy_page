79b097cde1708afabb45dfabe02595ebe5f67cb4
Samansum <40276693+Samansum@users.noreply.github.com>
2018-06-20 21:07:35 +0700

    Adding me.js and modify some of css and html code

---
+++ me.js
@@ -0,0 +1,7 @@
+function TestFun(){
+    var x = document.getElementById("euro_bread");
+    if(x.style.display == "none")
+    x.style.display = "block";
+    else
+    x.style.display = "none";
+}
